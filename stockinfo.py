43d6dc4a502ef357537f5eb84618495b4fa96226
Steven <bishopsscope@csu.fullerton.edu>
2022-07-13 22:23:05 -0700

    First commit

--- stockinfo.py
+++ stockinfo.py
@@ -1,26 +1,14 @@
 import yfinance as yf
 import json
 
-# Getting stock tickers from json included in folder
-f = open('DOW.json')
-data = json.load(f)
+def getJSONData(file: str) -> dict:
 
-# Ask the user the number of stocks they would like to look at
-stockNumber = int(input("Enter the number of stocks in Dow Jones you would \
-like to check (Insert a number between 1 and 30 inclusive): "))
+    # Getting stock tickers from json included in folder
+    # f = open('DOW.json')
+    f = open(file)
+    data = json.load(f)
 
-# If a number under or over the suggested amount will enter a loop until
-# the valid amount is insterted.
-while (stockNumber < 1 or stockNumber > 30):
-    stockNumber = int(input("Invalid Amount! Enter the number of stocks in \
-Dow Jones you would like to check (Insert a number between 1 and 30 \
-inclusive): "))
-
-# Create the list based off the amount the user inserted.
-listOfStocks = data['DOW'][0:(stockNumber)]
-
-print("\nGetting stock information. This may take a couple minutes depending \
-on the number of stocks selected.")
+    return data
 
 # This function will check if there is a valid key.
 def findInfo(currentStock, key):
@@ -29,30 +17,92 @@ def findInfo(currentStock, key):
     else:
         return None
 
-# We have a dictionary for assigning the information to the ticker called dowJones
-# There is also a list called keys for the keys we are looking for.
-dowJones = {}
-keys = ['open', 'previousClose', 'bid', 'ask', 'volume', 'trailingPE', 'forwardEps']
+def getStockNames(stockNumber: int, data: dict) -> list:
 
-# This loop will get all the stock tickers and assign all the information to the
-# tickers in the dictionary.
-for stock in listOfStocks:
-    stockInfo = []
-    print(stock)
-    currentStock = (yf.Ticker(stock)).info
-    for key in keys:
-        stockInfo.append(findInfo(currentStock, key))
+    # This is the array of stock names that will be returned from this function
+    stockNameList = []
+    stockInfoList = []
 
-    dowJones[stock] = stockInfo
+    # Return an empty list if the number of stocks is <= 0
+    if stockNumber <= 0 or stockNumber > 30:
 
-selectedStock = input("Enter the stock ticker from the list you would like \
-to look at or type 'exit' to end the program: ")
+        return []
 
-while (selectedStock != "exit"):
-    if selectedStock in listOfStocks:
-        print("{}: {}".format(selectedStock, dowJones[selectedStock]))
-    else:
-        print("Invalid ticker!")
+    # Create the list based off the amount the user inserted.
+    listOfStocks = data['DOW'][0:(stockNumber)]
+
+    # We have a dictionary for assigning the information to the ticker called dowJones
+    # There is also a list called keys for the keys we are looking for.
+    # dowJones = {}
+    keys = ['open', 'previousClose', 'bid', 'ask', 'volume', 'trailingPE', 'trailingEps']
+
+    for stock in listOfStocks:
+        # stockInfo = []
+        print(stock)
+        stockNameList.append(stock)
+        currentStock = (yf.Ticker(stock)).info
+        values = []
+        for key in keys:
+            values.append(findInfo(currentStock, key))
+
+        stockInfoList.append(values)
+
+        # dowJones[stock] = stockInfoList
+
+    return [stockNameList, stockInfoList]
+
+# Call the functions
+
+data = getJSONData('DOW.json')
+
+print(getStockNames(8, data))
+
+
+"""def getStockInfo(stockNumber: int)
+
+    # Ask the user the number of stocks they would like to look at
+    stockNumber = int(input("Enter the number of stocks in Dow Jones you would \
+    like to check (Insert a number between 1 and 30 inclusive): "))
+
+    # If a number under or over the suggested amount will enter a loop until
+    # the valid amount is insterted.
+    while (stockNumber < 1 or stockNumber > 30):
+        stockNumber = int(input("Invalid Amount! Enter the number of stocks in \
+    Dow Jones you would like to check (Insert a number between 1 and 30 \
+    inclusive): "))
+
+    # Create the list based off the amount the user inserted.
+    listOfStocks = data['DOW'][0:(stockNumber)]
+
+    print("\nGetting stock information. This may take a couple minutes depending \
+    on the number of stocks selected.")
+
+    # We have a dictionary for assigning the information to the ticker called dowJones
+    # There is also a list called keys for the keys we are looking for.
+    dowJones = {}
+    keys = ['open', 'previousClose', 'bid', 'ask', 'volume', 'trailingPE', 'forwardEps']
+
+    # This loop will get all the stock tickers and assign all the information to the
+    # tickers in the dictionary.
+    for stock in listOfStocks:
+        stockInfo = []
+        print(stock)
+        currentStock = (yf.Ticker(stock)).info
+        for key in keys:
+            stockInfo.append(findInfo(currentStock, key))
+
+        dowJones[stock] = stockInfo
 
     selectedStock = input("Enter the stock ticker from the list you would like \
-to look at or type 'exit' to end the program: ")
+    to look at or type 'exit' to end the program: ")
+
+    while (selectedStock != "exit"):
+        if selectedStock in listOfStocks:
+            print("{}: {}".format(selectedStock, dowJones[selectedStock]))
+        else:
+            print("Invalid ticker!")
+
+        selectedStock = input("Enter the stock ticker from the list you would like \
+    to look at or type 'exit' to end the program: ")"""
+
+
---
+++ test_unittest.py
@@ -0,0 +1,49 @@
+import unittest
+import stockinfo
+
+class TestStock(unittest.TestCase):
+
+    def testGetJSONData(self):
+
+        # This function tests if 1) the specified JSON file exists and 2) if JSON data could be loaded from that file
+
+        jsonData = stockinfo.getJSONData('DOW.json')
+
+        self.assertNotEqual(jsonData, None)
+
+    def testDidGetStock(self):
+
+        # This function tests to make sure that getStockNames can retrieve at least one stock
+
+        jsonData = stockinfo.getJSONData('DOW.json')
+
+        numStocks = 1
+
+        self.assertNotEqual(stockinfo.getStockNames(numStocks, jsonData), [])
+
+    def testDidGetNStocks(self):
+
+        jsonData = stockinfo.getJSONData('DOW.json')
+
+        numStocks = 8
+
+        self.assertEqual(len(stockinfo.getStockNames(numStocks, jsonData)[0]), numStocks)
+
+    def testInvalidStockNumber(self):
+
+        jsonData = stockinfo.getJSONData('DOW.json')
+
+        numStocks = 0
+
+        self.assertEqual(stockinfo.getStockNames(numStocks, jsonData), [])
+
+        numStocks = -1
+
+        self.assertEqual(stockinfo.getStockNames(numStocks, jsonData), [])
+
+        numStocks = 31
+
+        self.assertEqual(stockinfo.getStockNames(numStocks, jsonData), [])
+
+if __name__ == '__main__':
+    unittest.main()
