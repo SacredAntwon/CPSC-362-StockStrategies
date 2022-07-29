d2bda865cf9b320ea54323f262a900316cc3b63f
SacredAntwon <antwonm9898@gmail.com>
2022-07-28 22:09:24 -0700

    bunch of updates

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import stockinfo
 import backtesting
-#import backtest
+import smacro
+import meanrev
+#from backtesting import Backtest, Strategy
 # Call the functions
+meanr = meanrev.MeanReversion()
 
 obj = stockinfo.StockInfo()
-# backt = backtest.BackTest()
+#backt = backtest.SMAcross()
 
+#bt = Backtest(obj.getStockHistory('IBM'), backt, cash=100000, commission = 0.002, exclusive_orders=True)
 print("Getting stock information from Yahoo Finance. Please wait a few seconds!")
 
 obj.readJSONFile()
@@ -17,15 +21,15 @@ for name in allStocks:
     print(name)
 
 # Get the list of prices (this information will be passed to the backtesting class)
-prices = obj.getStockHistory("IBM")["Open"]
+######prices = obj.getStockHistory("IBM")["Open"]
 
 #print(prices)
 
-bt = backtesting.Backtesting(10000, prices, 0)
+######bt = backtesting.Backtesting(10000, prices, 0)
 
 # Uncomment this after you're done testing
 
-"""print(len(prices))
+#print(len(prices))
 
 print("Select a stock from the list above.")
 userStock = input("\nType the ticker you would like to view more information \
@@ -36,11 +40,11 @@ while (userStock != 'exit'):
         print("\nNot A Valid Ticker!")
     else:
         print(obj.displayInfo(userStock))
-        print(backt.movingAverage(userStock))
+        #print(backt.movingAverage(userStock))
         #print(obj.getStockHistory(userStock))
         #stock = obj.getStockHistory(userStock)
         #print(stock)
         #backtest.getStats(stock)
         #print(backtest.getStats(stock))
     userStock = input("Type the ticker you would like to view more information \
-about(Case Sensitive) or 'exit' to end the program: ")"""
+about(Case Sensitive) or 'exit' to end the program: ")
