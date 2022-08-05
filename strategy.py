2e6d1835aeaedfc90625dd29cd0a70f7df8a2dff
SacredAntwon <antwonm9898@gmail.com>
2022-08-04 19:03:02 -0700

    update

--- strategy.py
+++ strategy.py
@@ -5,7 +5,11 @@ from backtesting import Backtest, Strategy
 from backtesting.lib import crossover, SignalStrategy, TrailingStrategy
 from backtesting.test import SMA
 
-class SmaCross(Strategy):
+class InterfaceSMACross(Strategy):
+    def init(self):
+        pass
+
+class SmaCross(InterfaceSMACross):
     def init(self):
         price = self.data.Close
         self.ma1 = self.I(SMA, price, 50)
@@ -17,7 +21,7 @@ class SmaCross(Strategy):
         elif crossover(self.ma2, self.ma1):
             self.sell()
 
-class SmaCross2(Strategy):
+class SmaCross2(InterfaceSMACross):
     def init(self):
         self.price = self.data.Close
         self.ma1 = self.I(SMA, self.price, 50)
@@ -29,35 +33,35 @@ class SmaCross2(Strategy):
         elif crossover(self.price, self.ma2):
             self.sell()
 
-class SigTrailCross(SignalStrategy,
-               TrailingStrategy):
-    n1 = 10
-    n2 = 20
-
-    def init(self):
-        # In init() and in next() it is important to call the
-        # super method to properly initialize the parent classes
-        super().init()
-
-        # Precompute the two moving averages
-        sma1 = self.I(SMA, self.data.Close, self.n1)
-        sma2 = self.I(SMA, self.data.Close, self.n2)
-
-        # Where sma1 crosses sma2 upwards. Diff gives us [-1,0, *1*]
-        signal = (pd.Series(sma1) > sma2).astype(int).diff().fillna(0)
-        signal = signal.replace(-1, 0)  # Upwards/long only
-
-        # Use 95% of available liquidity (at the time) on each order.
-        # (Leaving a value of 1. would instead buy a single share.)
-        entry_size = signal * .95
-
-        # Set order entry sizes using the method provided by
-        # `SignalStrategy`. See the docs.
-        self.set_signal(entry_size=entry_size)
-
-        # Set trailing stop-loss to 2x ATR using
-        # the method provided by `TrailingStrategy`
-        self.set_trailing_sl(2)
+# class SigTrailCross(SignalStrategy,
+#                TrailingStrategy):
+#     n1 = 10
+#     n2 = 20
+#
+#     def init(self):
+#         # In init() and in next() it is important to call the
+#         # super method to properly initialize the parent classes
+#         super().init()
+#
+#         # Precompute the two moving averages
+#         sma1 = self.I(SMA, self.data.Close, self.n1)
+#         sma2 = self.I(SMA, self.data.Close, self.n2)
+#
+#         # Where sma1 crosses sma2 upwards. Diff gives us [-1,0, *1*]
+#         signal = (pd.Series(sma1) > sma2).astype(int).diff().fillna(0)
+#         signal = signal.replace(-1, 0)  # Upwards/long only
+#
+#         # Use 95% of available liquidity (at the time) on each order.
+#         # (Leaving a value of 1. would instead buy a single share.)
+#         entry_size = signal * .95
+#
+#         # Set order entry sizes using the method provided by
+#         # `SignalStrategy`. See the docs.
+#         self.set_signal(entry_size=entry_size)
+#
+#         # Set trailing stop-loss to 2x ATR using
+#         # the method provided by `TrailingStrategy`
+#         self.set_trailing_sl(2)
 
 def grabStrategyInfo(tickers, cash):
     obj = stockinfo.StockInfo()
@@ -66,14 +70,18 @@ def grabStrategyInfo(tickers, cash):
         stratDict = {}
         for item in tickers:
             stock = stockinfo.getHistoricalData(item)
+            interface = InterfaceSMACross()
+            interface2 = InterfaceSMACross()
             statsSMA = Backtest(stock, SmaCross, cash=money, commission= 0, exclusive_orders=True)
-            statsSigTrail = Backtest(stock, SmaCross2, cash=money, commission= 0, exclusive_orders=True)
-            statsSMA = statsSMA.run()
-            statsSigTrail = statsSigTrail.run()
+            statsSMA2 = Backtest(stock, SmaCross2, cash=money, commission= 0, exclusive_orders=True)
+            interface = statsSMA
+            interface2 = statsSMA2
+            interface = interface.run()
+            interface2 = interface2.run()
 
             stratDict[item] = {}
-            stratDict[item]["Trend-Following"] = obj.keepImportantInfo(statsSMA)
-            stratDict[item]["Signal-And-Trailing"] = obj.keepImportantInfo(statsSigTrail)
+            stratDict[item]["Trend-Following"] = obj.keepImportantInfo(interface)
+            stratDict[item]["Signal-And-Trailing"] = obj.keepImportantInfo(interface2)
 
         obj.jsonFileDump("userStrategies.json", stratDict)
 
